bd9b46744794aea854063d00a90bdb558f85cae8
Bartek71 <getlowmasterpiece@gmail.com>
2021-11-25 00:50:00 +0000

    BindProperties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bind';
+ 
+  allowNewServer = false;
+
+    constructor() {
+
+      setTimeout(() => {
+        this.allowNewServer = true;
+      }, 2000);
+
+    
+
+    }
+
+
+
+
 }
